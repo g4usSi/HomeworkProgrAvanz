43dab0cfb4b74016eb6100b5d9e8632e2c468580
Geovanny <geovanny.alcon2@gmail.com>
2024-07-25 18:24:43 -0600

    Corregidas funciones, y agregue el metodo de mostrar la lista

--- FuncionesyMetodos/Program.cs
+++ FuncionesyMetodos/Program.cs
@@ -13,12 +13,14 @@ namespace CalculadoraEstadistica
             do 
             {
                 ImprimirMenu();
-                switch (opcion = PedirNumero()) 
+                switch (opcion = LlenarNumero()) 
                 {
                 case 1:
-                    Console.WriteLine("Opcion 1 uwu");                        
+                    Console.WriteLine();
+                    LlenarLista();
                     break;
                 case 2:
+                    
                     break;
                 case 3:
                     break;
@@ -26,9 +28,12 @@ namespace CalculadoraEstadistica
                     break; 
                 case 5:
                     break;
+                case 6:
+                    MostrarLista();
+                    break;
                 default:
                     Console.Clear();
-                    Console.WriteLine(">>>Opcion Invalida");
+                    Console.WriteLine("> > > Opcion Invalida");
                     break;
                 }
                 
@@ -43,10 +48,37 @@ namespace CalculadoraEstadistica
             Console.WriteLine("[3] Calcular la mediana");
             Console.WriteLine("[4] Calcular la desviacion estandar");
             Console.WriteLine("[5] Finalizar programa");
+            Console.WriteLine("[6] Imprimir lista");
             Console.Write("Ingrese una opcion: ");
         }
-        static int PedirNumero() 
+        static void LlenarLista() 
         {
+            //Se limpia la lista
+            ListaNumeros.Clear();
+
+            Console.Write("Ingrese el tamaño de los datos que desea llenar: ");
+            int tamaño = LlenarNumero();  
+            
+                for (int i = 0 ; i < tamaño ; i++) 
+                {
+                    Console.Write($"Ingrese el numero en la posicion ({i + 1}): ");
+                    int numero = LlenarNumero();
+                    ListaNumeros.Add(numero);
+                }
+
+            Console.WriteLine();
+        }
+
+        static void MostrarLista() 
+        {
+            Console.WriteLine("\nLista de numeros: ");
+                foreach (int i in ListaNumeros) {
+                    Console.Write(i + " ");
+                }
+            Console.WriteLine("\n");
+        }
+        static int LlenarNumero()
+        {//Llenar numero
             bool valido = false;
             while (!valido)
             {
@@ -58,16 +90,19 @@ namespace CalculadoraEstadistica
                 }
                 catch (FormatException ex)
                 {
-                    Console.WriteLine("Error no puede ingresar letras. Intente de nuevo: " + ex);
+                    Console.WriteLine("[!] Error no puede ingresar letras...");
+                    Console.Write("Intente de nuevo: ");
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Error desconocido: " + ex);
+                    Console.WriteLine("[!] Error desconocido... ");
+                    Console.Write("> Intente de nuevo: ");
                 }
             }
             return numero;
         }
-        
+
+
 
     }
 }
--- FuncionesyMetodos/obj/Debug/net8.0/FuncionesyMetodos.AssemblyInfo.cs
+++ FuncionesyMetodos/obj/Debug/net8.0/FuncionesyMetodos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b6247a0836fe48a5e10f75916a113e76a81e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012dfa4fd88b1bbc840714c652549b8178fcff4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
