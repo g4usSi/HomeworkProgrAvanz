07fa6778a82916350f19640027cb06673e8a7f97
Geovanny <geovanny.alcon2@gmail.com>
2024-07-25 20:00:26 -0600

    Metodo para mediana

--- FuncionesyMetodos/Program.cs
+++ FuncionesyMetodos/Program.cs
@@ -5,31 +5,36 @@ namespace CalculadoraEstadistica
 {
     internal class Program 
     { 
-        static List<int> ListaNumeros = new List<int>();
-        static int numero;
-        static int resultado = 0;
-        static void Main(string[] arg) {
+        static List<double> ListaNumeros = new List<double>();
+        static double numero;
+        static double resultado;
+        static int num;
+        static void Main(string[] args) {
             int opcion;
             do 
             {
                 ImprimirMenu();
-                switch (opcion = LlenarNumero()) 
+                switch (opcion = LlenarNumeroEntero()) 
                 {
                 case 1:
                     Console.WriteLine();
                     LlenarLista();
                     break;
                 case 2:
-                    
-                    break;
+                    ImprimirLista();
+                    Media();
+                    Console.WriteLine("El resultado de la media es: "+ resultado);
+                    Console.WriteLine();
+                        break;
                 case 3:
+                    Mediana();
                     break;
                 case 4:
                     break; 
                 case 5:
                     break;
                 case 6:
-                    MostrarLista();
+                    ImprimirLista();
                     break;
                 default:
                     Console.Clear();
@@ -57,34 +62,70 @@ namespace CalculadoraEstadistica
             ListaNumeros.Clear();
 
             Console.Write("Ingrese el tamaño de los datos que desea llenar: ");
-            int tamaño = LlenarNumero();  
+            double tamaño = LlenarNumero();  
             
                 for (int i = 0 ; i < tamaño ; i++) 
                 {
                     Console.Write($"Ingrese el numero en la posicion ({i + 1}): ");
-                    int numero = LlenarNumero();
-                    ListaNumeros.Add(numero);
+                    double a = LlenarNumero();
+                    ListaNumeros.Add(a);
                 }
-
+            
             Console.WriteLine();
         }
 
-        static void MostrarLista() 
+        static void ImprimirLista() 
         {
             Console.WriteLine("\nLista de numeros: ");
+            Console.Write("\t\t");
                 foreach (int i in ListaNumeros) {
                     Console.Write(i + " ");
                 }
             Console.WriteLine("\n");
         }
-        static int LlenarNumero()
+
+        static double Media()
+        {
+            double sumaNumeros = 0;
+            foreach (int i in ListaNumeros)
+            {
+
+                sumaNumeros += i;
+            }
+            Console.WriteLine(sumaNumeros);
+            resultado = (sumaNumeros / ListaNumeros.Count);
+            return resultado;
+        }
+
+        static void Mediana() 
+        {
+            ListaNumeros.Sort();
+            ImprimirLista();
+            int puntoMedio = ListaNumeros.Count / 2;
+
+            if (ListaNumeros.Count % 2 == 0)
+            {
+                double medianaLista = (ListaNumeros[puntoMedio] + ListaNumeros[puntoMedio-1]) / 2;
+                Console.WriteLine("La media para la lista es: "+ medianaLista);
+            }
+            else 
+            {
+                Console.WriteLine("La media para la lista es: " + ListaNumeros[puntoMedio]);
+            }
+        }
+        static void Moda()
+        { 
+
+        }
+
+        static double LlenarNumero()
         {//Llenar numero
             bool valido = false;
             while (!valido)
             {
                 try
                 {
-                    numero = Convert.ToInt32(Console.ReadLine());
+                    numero = Convert.ToDouble(Console.ReadLine());
                     //se ejecuta linea por linea si hay una excepcion se captura antes del bool
                     valido = true;
                 }
@@ -101,7 +142,30 @@ namespace CalculadoraEstadistica
             }
             return numero;
         }
-
+        static int LlenarNumeroEntero()
+        {//Llenar numero
+            bool valido = false;
+            while (!valido)
+            {
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                    //se ejecuta linea por linea si hay una excepcion se captura antes del bool
+                    valido = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("[!] Error no puede ingresar letras...");
+                    Console.Write("Intente de nuevo: ");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[!] Error desconocido... ");
+                    Console.Write("> Intente de nuevo: ");
+                }
+            }
+            return num;
+        }
 
 
     }
--- FuncionesyMetodos/obj/Debug/net8.0/FuncionesyMetodos.AssemblyInfo.cs
+++ FuncionesyMetodos/obj/Debug/net8.0/FuncionesyMetodos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012dfa4fd88b1bbc840714c652549b8178fcff4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43dab0cfb4b74016eb6100b5d9e8632e2c468580")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
