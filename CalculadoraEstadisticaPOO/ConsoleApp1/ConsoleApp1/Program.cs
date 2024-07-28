9680dd8e8f089a0c6773613d94e8f88a422024fc
Geovanny <geovanny.alcon2@gmail.com>
2024-07-28 10:50:26 -0600

    Apartado de la calculadora con base a POO

---
+++ CalculadoraEstadisticaPOO/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp1;
+
+public class Program {
+    public static void Main(string[] args) 
+    {
+        Calculadora calculadora = new Calculadora();
+        Console.WriteLine();
+
+    }
+}
