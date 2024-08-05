ab0e106e983935c79e189d43d0c7eb8a3380c7cb
Geovanny <geovanny.alcon2@gmail.com>
2024-08-05 11:51:35 -0600

    Cambios de visual

--- FuncionesyMetodos/obj/Debug/net8.0/FuncionesyMetodos.AssemblyInfo.cs
+++ FuncionesyMetodos/obj/Debug/net8.0/FuncionesyMetodos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfaa7b66c506e70694d1ee496ea2755fea058bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28136e9486691a7d687c1e672f1892dc7650cf85")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuncionesyMetodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
